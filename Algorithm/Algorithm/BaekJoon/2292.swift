7abdde549e888365650ee3488b1d72bd28c40fe4
Mr. Remark <itsmine096@gmail.com>
2022-01-01 09:26:31 +0900

    BaekJoon 2292

---
+++ Algorithm/Algorithm/BaekJoon/2292.swift
@@ -0,0 +1,26 @@
+//
+//  main.swift
+//  Algorithm
+//
+//  Created by Hyun on 2022/01/01.
+//
+
+import Foundation
+
+var inputNum = Int(readLine()!)!
+var count = 1
+var room = 1
+
+if inputNum == 1 {
+    print(count)
+}
+else {
+    repeat{
+        room = room + (count * 6)
+        count += 1
+    }while inputNum > room
+    print(count)
+}
+
+
+
