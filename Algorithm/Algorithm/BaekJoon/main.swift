f1e52bce85977e44fe6ea41f66c12919a5fb6fb2
Mr. Remark <itsmine096@gmail.com>
2022-01-08 21:59:38 +0900

    BaekJoon(2896) 푸는 중

--- Algorithm/Algorithm/BaekJoon/main.swift
+++ Algorithm/Algorithm/BaekJoon/main.swift
@@ -5,6 +5,28 @@
 //  Created by Hyun on 2022/01/03.
 //
 
-import Foundation
+var inputNum = readLine()!.split(separator: " ").map{ Int($0)! }
 
-print("Hello, World")
+var a = inputNum[0]
+var b = inputNum[1]
+var v = inputNum[2]
+
+if a == v {
+    print(1)
+}
+else if v / a == 1 {
+    print(2)
+}
+else{
+    if v % (a-b) == 0 {
+        let days = v / (a-b)
+        
+        // 3 1 10
+        // 3 2 - 5 4 - 7 6 - 9 8 - 11 10
+    }
+    else {
+        
+    }
+    
+}
+// 5 3 - 8 5 - 10
