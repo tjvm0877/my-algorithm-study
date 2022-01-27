543d93c537792ea47a0d47a5b75462e851b0b627
Mr. Remark <itsmine096@gmail.com>
2022-01-27 17:22:50 +0900

    Boj 2869

---
+++ Algorithm/Algorithm/Boj/2869.swift
@@ -0,0 +1,9 @@
+//  Created by Hyun on 2022/01/27.
+import Foundation
+let arr = readLine()!.split(separator: " ").map{Double($0)!}
+let A = arr[0]
+let B = arr[1]
+let V = arr[2]
+
+var x = ceil((V - B) / (A - B))
+print("\(Int(x))")
