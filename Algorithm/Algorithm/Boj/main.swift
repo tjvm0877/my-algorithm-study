c0c4b72736d58c395615e3f649edc935bcfaab56
Mr. Remark <itsmine096@gmail.com>
2022-04-06 23:06:23 +0900

    Boj2581

---
+++ Algorithm/Algorithm/Boj/main.swift
@@ -0,0 +1,36 @@
+//  Created by Hyun on 2022/04/06.
+let M = Int(readLine()!)!
+let N = Int(readLine()!)!
+
+var sum = 0
+var isPrimeNumber = true
+var allPrimeNumber = false
+var arr: [Int] = []
+
+for i in M...N {
+    if i == 1 {
+        isPrimeNumber = false
+    }else {
+        for j in 2..<i {
+            if i % j == 0 {
+                isPrimeNumber = false
+            }
+        }
+        
+        if isPrimeNumber == true {
+            arr.append(i)
+            allPrimeNumber = true
+        }
+    }
+    isPrimeNumber = true
+}
+
+if allPrimeNumber {
+    for k in 0..<arr.count {
+        sum += arr[k]
+    }
+    print("\(sum)")
+    print("\(arr.min()!)")
+}else {
+    print("-1")
+}
