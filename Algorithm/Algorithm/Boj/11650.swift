a985352eeec0c4f3c307387f662334db0360cab7
Mr. Remark <itsmine096@gmail.com>
2022-04-11 11:33:55 +0900

    Boj11650

---
+++ Algorithm/Algorithm/Boj/11650.swift
@@ -0,0 +1,19 @@
+//  Created by Hyun on 2022/04/11.
+let N = Int(readLine()!)!
+var axis: [[Int]] = []
+
+for _ in 0..<N {
+    let input = readLine()!.split(separator: " ").map{ Int($0)! }
+    axis.append(input)
+}
+
+axis.sort{ (a,b) -> Bool in
+    if a[0] == b[0] {
+        return a[1] < b[1]
+    }
+    return a[0] < b[0]
+}
+
+for i in 0..<N {
+    print("\(axis[i][0]) \(axis[i][1])")
+}
