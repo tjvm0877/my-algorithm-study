50213cf3b6262a75eaa5fb71429230b13a2644aa
Mr. Remark <itsmine096@gmail.com>
2022-04-18 09:06:44 +0900

    Boj 1000(Kotlin)

---
+++ Kotlin_Algorithm/src/Boj/1000.kt
@@ -0,0 +1,13 @@
+package Boj
+
+import java.util.*
+
+fun main() {
+    val sc: Scanner = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    val result = a + b
+    println(result)
+}
