5695d982cea2b26de1a80d1704eb90dd44573f41
Mr. Remark <itsmine096@gmail.com>
2022-04-23 18:53:24 +0900

    Boj1330

---
+++ Kotlin_Algorithm/src/Boj/1330.kt
@@ -0,0 +1,19 @@
+package Boj
+
+import java.util.*
+
+fun main(){
+    val sc:Scanner = Scanner(System.`in`)
+
+    val input = sc.nextLine().split(" ")
+    val a = input[0].toInt()
+    val b = input[1].toInt()
+
+    if(a>b) {
+        println(">")
+    } else if (a==b) {
+        println("==")
+    } else {
+        println("<")
+    }
+}
