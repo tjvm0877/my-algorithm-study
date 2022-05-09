fc2d8bbcac8737d95921bd91beaa9c616db9a30a
Mr. Remark <itsmine096@gmail.com>
2022-05-09 20:48:20 +0900

    Boj1330(Java)

---
+++ Kotlin_Algorithm/src/Java/Boj_1330.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_1330 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        br.close();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        if (a > b) {
+            System.out.println(">");
+        } else if(a == b) {
+            System.out.println("==");
+        } else {
+            System.out.println("<");
+        }
+    }
+}
