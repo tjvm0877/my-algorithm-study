31d25fdb63151da42e07197ea61b5366396a312b
Mr. Remark <itsmine096@gmail.com>
2022-05-09 20:38:26 +0900

    Boj_2739(Java) Refactor

--- Kotlin_Algorithm/src/Java/Boj_2739.java
+++ Kotlin_Algorithm/src/Java/Boj_2739.java
@@ -7,8 +7,8 @@ import java.io.InputStreamReader;
 public class Boj_2739 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int n = Integer.parseInt(str);
+        int n = Integer.parseInt(br.readLine());
+        br.close();
 
         for(int i = 1; i < 10; i++) {
             System.out.println(String.format("%d * %d = %d", n, i, i*n));
