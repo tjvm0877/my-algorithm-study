3641df66b98874aefbef88316466e7a1bb69f495
Mr. Remark <itsmine096@gmail.com>
2022-05-09 20:34:50 +0900

    Boj2739(Java)

---
+++ Kotlin_Algorithm/src/Java/Boj_2739.java
@@ -0,0 +1,17 @@
+package Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj_2739 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        int n = Integer.parseInt(str);
+
+        for(int i = 1; i < 10; i++) {
+            System.out.println(String.format("%d * %d = %d", n, i, i*n));
+        }
+    }
+}
