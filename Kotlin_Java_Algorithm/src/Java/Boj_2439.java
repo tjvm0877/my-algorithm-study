232e77761c90bcaecfed6048dc23477e4b9940f5
Hyun <itsmine096@gmail.com>
2022-05-12 22:05:50 +0900

    Boj2439(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Boj_2439.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// Boj2439
+public class Boj_2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        br.close();
+
+        for (int i = 1; i <= n; i++) {
+            String stars = "";
+            for (int j = 1; j <= (n-i); j++) {
+                stars += " ";
+            }
+            for (int k = 1; k <= i; k++) {
+                stars += "*";
+            }
+            System.out.println(stars);
+        }
+    }
+}
