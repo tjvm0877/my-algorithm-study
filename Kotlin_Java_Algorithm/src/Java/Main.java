e0f56c0c78ec0f65e40ab1039cc92bb35c92b40d
Hyun <itsmine096@gmail.com>
2022-05-16 09:23:54 +0900

    Boj10818(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Main.java
@@ -0,0 +1,27 @@
+package Java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stN = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(stN.nextToken());
+
+        StringTokenizer stA = new StringTokenizer(br.readLine());
+        int numArr[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            numArr[i] = Integer.parseInt(stA.nextToken());
+        }
+
+        Arrays.sort(numArr);
+
+        System.out.println(String.format(("%d %d"), numArr[0], numArr[n - 1]));
+    }
+}
