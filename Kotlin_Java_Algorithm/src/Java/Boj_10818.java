140bff7bab306714ad00e557f670213fc5ba1c8f
Hyun <itsmine096@gmail.com>
2022-05-16 09:25:32 +0900

    Rename(Main -> Boj_10818)

--- Kotlin_Java_Algorithm/src/Java/Boj_10818.java
+++ Kotlin_Java_Algorithm/src/Java/Boj_10818.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Boj_10818 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer stN = new StringTokenizer(br.readLine());
