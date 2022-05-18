cfdce6bfc05536365fa2e952ad72871b8399dca8
Hyun <itsmine096@gmail.com>
2022-05-18 13:49:26 +0900

    Boj2562(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Boj_2562.java
@@ -0,0 +1,27 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Boj_2562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numArr[] = new int[9]; // 크기 9인 배열선언
+        int max = 0;
+        int maxLoc = 0;
+
+        // 입력, 입력받은 값 배열에 저장
+        for (int i = 0; i < numArr.length; i++) {
+            numArr[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < numArr.length; i++) {
+            if (numArr[i] > max) {
+                max = numArr[i];
+                maxLoc = i;
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(maxLoc + 1);
+    }
+}
