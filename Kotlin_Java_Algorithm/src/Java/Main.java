6ed076e8472b17543348e8fdb9c89698b743b9c8
Hyun <itsmine096@gmail.com>
2022-05-19 21:31:22 +0900

    Boj8958(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Main.java
@@ -0,0 +1,31 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sr = new Scanner(System.in);
+        String arr[] = new String[sr.nextInt()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sr.next();
+        }
+        sr.close();
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            int sum = 0;
+
+            for (int j = 0; j < arr[i].length(); j++) {
+                if (arr[i].charAt(j) == 'O') {
+                    count++;
+                } else {
+                    count = 0;
+                }
+                sum += count;
+            }
+
+            System.out.println(sum);
+        }
+    }
+}
