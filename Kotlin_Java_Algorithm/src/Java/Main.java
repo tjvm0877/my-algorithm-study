38c4974577a2b9e037683ed60da930ba0080ca6c
Hyun <itsmine096@gmail.com>
2022-05-20 23:33:54 +0900

    no message

---
+++ Kotlin_Java_Algorithm/src/Java/Main.java
@@ -0,0 +1,16 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int c = sc.nextInt();
+        int arr[] = new int[c];
+        for (int i = 0; i < c; i++) {
+
+        }
+
+    }
+}
