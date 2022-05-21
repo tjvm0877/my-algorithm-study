55e2917b8a2fa9ec5ddc48fcd14ef81d173fb047
Hyun <itsmine096@gmail.com>
2022-05-21 20:19:53 +0900

    Boj3502(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Boj_3502.java
@@ -0,0 +1,18 @@
+package Java;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Boj_3502 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        HashSet<Integer> h = new HashSet<Integer>();
+
+        for (int i = 0; i < 10; i++) {
+            h.add(sc.nextInt() % 42);
+        }
+
+        sc.close();
+        System.out.println(h.size());
+    }
+}
