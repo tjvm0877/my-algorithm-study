75aeb91a437edf9d5416dc6fe7ffccd4e752d46d
Hyun <itsmine096@gmail.com>
2022-05-23 23:36:53 +0900

    Delete Remark

--- Kotlin_Java_Algorithm/src/Java/Boj_2577.java
+++ Kotlin_Java_Algorithm/src/Java/Boj_2577.java
@@ -6,7 +6,6 @@ public class Boj_2577 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 배열 선언, 기본 자료형의 경우 초기값이 0
         int numArr[] = new int[10];
 
         int a = sc.nextInt();
