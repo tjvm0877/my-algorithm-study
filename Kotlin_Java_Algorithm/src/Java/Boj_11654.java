44be4c5297e5f738219a74e62129610f335b4491
Hyun <itsmine096@gmail.com>
2022-05-29 23:07:05 +0900

    Boj11654(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Boj_11654.java
@@ -0,0 +1,11 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Boj_11654 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.next().charAt(0);
+        System.out.println(n);
+    }
+}
