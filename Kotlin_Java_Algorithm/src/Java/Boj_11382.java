197e449dacc16839e8c97b1c5a709f1272461141
Hyun <itsmine096@gmail.com>
2022-06-21 09:27:49 +0900

    Boj11382(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Boj_11382.java
@@ -0,0 +1,14 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Boj_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+
+        System.out.println(a + b + c);
+    }
+}
