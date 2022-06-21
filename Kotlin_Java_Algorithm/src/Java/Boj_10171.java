98bb07dd189797b097281b51a31d62eafe1af1ee
Hyun <itsmine096@gmail.com>
2022-06-21 09:19:08 +0900

    Boj10171(Java)

---
+++ Kotlin_Java_Algorithm/src/Java/Boj_10171.java
@@ -0,0 +1,10 @@
+package Java;
+
+public class Boj_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|\n");
+    }
+}
